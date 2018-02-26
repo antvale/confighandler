e385e458f4b2b525d9b7f3e477153550f7f0fd35
antvale <ant.valentino@gmail.com>
2018-02-27 00:15:12 +0100

    added createGraph and some traversal queries

--- poc.tinkerpop-server/src/main/java/org/dse/example/DSEGraphRemoteApp.java
+++ poc.tinkerpop-server/src/main/java/org/dse/example/DSEGraphRemoteApp.java
@@ -2,11 +2,17 @@ package org.dse.example;
 
 import java.io.Closeable;
 import java.io.IOException;
+
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+
+import org.apache.tinkerpop.gremlin.structure.Graph;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
+
 import org.apache.commons.configuration.Configuration;
 import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.tinkerpop.gremlin.process.traversal.Traversal;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
 
 import com.datastax.driver.dse.DseCluster;
@@ -14,8 +20,14 @@ import com.datastax.driver.dse.DseSession;
 import com.datastax.driver.dse.graph.GraphOptions;
 import com.datastax.dse.graph.api.DseGraph;
 
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class DSEGraphRemoteApp {
-    //private static final Logger LOGGER = LoggerFactory.getLogger(DSEGraphRemoteApp.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(DSEGraphRemoteApp.class);
 
     static class Session implements Closeable {
         final GraphTraversalSource g;
@@ -46,17 +58,77 @@ public class DSEGraphRemoteApp {
 
     }
 
+
+    public static void createGraph(GraphTraversalSource g){
+        g.addV("SimpleCurrency").as("c")
+                .property("symbol","GBP")
+                .property("rank","1")
+                .group()
+                .next();
+    }
+
+    public static void createGraphFamily(GraphTraversalSource g){
+        final Graph gh = g.getGraph();
+
+        final Vertex marko = gh.addVertex("name");
+
+        final Vertex vadas = gh.addVertex("name", "vadas", "age", 27);
+        final Vertex lop = gh.addVertex("name", "lop", "lang", "java");
+        final Vertex josh = gh.addVertex("name", "josh", "age", 32);
+        final Vertex ripple = gh.addVertex("name", "ripple", "lang", "java");
+        final Vertex peter = gh.addVertex("name", "peter", "age", 35);
+        marko.addEdge("knows", vadas, "weight", 0.5f);
+        marko.addEdge("knows", josh, "weight", 1.0f);
+        marko.addEdge("created", lop, "weight", 0.4f);
+        josh.addEdge("created", ripple, "weight", 1.0f);
+        josh.addEdge("created", lop, "weight", 0.4f);
+        peter.addEdge("created", lop, "weight", 0.2f);
+    }
+
+
+
+
     public static void main(String[] args) throws Exception {
 
-        final Path confPath = Paths.get( "conf", "dse.properties");
+        final Path confPath = Paths.get("conf", "dse.properties");
 
         final Configuration conf = new PropertiesConfiguration(confPath.toFile());
 
-        try( final Session s =Session.open(conf.getString("host", "localhost"), conf.getString("graph","DSE_GRAPH_QUICKSTART"))) {
 
-                //s.g.V().count()
+        final Session s = Session.open(conf.getString("host", "192.168.1.13"), conf.getString("graph", "DSE_GRAPH_QUICKSTART"));
 
+        //createGraph(s.g);
+
+        //createGraphFamily(s.g);
+
+        LOGGER.info("Number of vertexes" + s.g.V().count().next());
+
+        Traversal<Vertex, String> t1 = s.g.V().values("symbol");
+
+        while (t1.hasNext()) {
+            LOGGER.info("value: " + t1.next());
+        }
+
+        Traversal<Vertex, String> t2 = s.g.V().hasLabel("SimpleCurrency").values();
+
+        while (t2.hasNext()) {
+            /*
+            Map<String, List> m= t2.next();
+            if (m.size()>0){
+                m.get(0)
+            }
+            */
+            LOGGER.info("value: " + t2.next());
+        }
+
+        Traversal<Vertex, Map<String, List>> t3 = s.g.V().hasLabel("SimpleCurrency").valueMap();
+
+        while (t3.hasNext()) {
+            Map<String, List> m = t3.next();
+            if (m.size() > 0) {
+                    for(String key: m.keySet())
+                         LOGGER.info("key: " + key + " value:"+m.get(key).get(0));
+            }
         }
     }
-
 }
